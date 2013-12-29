f895dfdb8d8a154454685a5f3b6eb984483a106f
snrdrmz <durmazsoner@gmail.com>
2013-12-29 10:54:07 +0200

    .

---
+++ scripts/ll.js
@@ -0,0 +1,5 @@
+var slides = {
+	desc: "Led Lambalar",
+	root: "images/led_lambalar/",
+	images: ["004.png", "005.png", "006.png", "007.png", "008.png", "009.png", "010.png", "011.png"]
+};
---
+++ scripts/tll.js
@@ -0,0 +1,5 @@
+var slides = {
+	desc: "Tekfen Serisi Led Lambalar",
+	root: "images/Tekfen_Serisi _Led_Lambalar/",
+	images: ["012.png", "013.png", "014.png", "015.png"]
+};
