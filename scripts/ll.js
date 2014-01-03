4c0328cc1276990468923596429478a5a673e13c
snrdrmz <durmazsoner@gmail.com>
2014-01-03 17:39:00 +0200

    .

--- scripts/ll.js
+++ scripts/ll.js
@@ -1,5 +1,5 @@
 var slides = {
-	desc: "Led Lambalar",
+	desc: "Katalog",
 	root: "images/led_lambalar/",
-	images: ["004.png", "005.png", "006.png", "007.png", "008.png", "009.png", "010.png", "011.png"]
+	images: ["004.png", "005.png", "006.png", "007.png", "008.png", "009.png", "010.png", "011.png","040.png", "041.png", "042.png", "043.png", "044.png", "045.png", "046.png", "047.png","048.png", "049.png", "050.png", "051.png", "052.png", "053.png", "054.png", "055.png","056.png", "057.png", "058.png", "059.png", "060.png", "061.png", "062.png", "063.png", "064.png", "065.png","066.png", "067.png", "068.png", "069.png", "070.png", "01.png", "072.png", "073.png"]
 };
