cd5b7538c0db0ad9d78d62b65c2565d5de131b96
snrdrmz <durmazsoner@gmail.com>
2014-01-13 00:54:49 +0200

    .

--- scripts/ll.js
+++ scripts/ll.js
@@ -1,5 +1,5 @@
 var slides = {
 	desc: "Katalog",
 	root: "images/led_lambalar/",
-	images: ["004.png", "005.png", "006.png", "007.png", "008.png", "009.png", "010.png", "011.png","040.png", "041.png", "042.png", "043.png", "044.png", "045.png", "046.png", "047.png","048.png", "049.png", "050.png", "051.png", "052.png", "053.png", "054.png", "055.png","056.png", "057.png", "058.png", "059.png", "060.png", "061.png", "062.png", "063.png", "064.png", "065.png","066.png", "067.png", "068.png", "069.png", "070.png", "071.png", "072.png", "073.png"]
+	images: ["Page_02.png","Page_03.png","Page_04.png","Page_05.png","Page_06.png","Page_07.png","Page_08.png","Page_09.png","Page_10.png","Page_11.png","Page_12.png","Page_13.png","Page_14.png","Page_15.png","Page_16.png","Page_17.png","Page_18.png","Page_19.png","Page_20.png","Page_21.png","Page_22.png","Page_23.png","Page_24.png","Page_25.png","Page_26.png","Page_27.png","Page_28.png","Page_29.png","Page_30.png","Page_31.png","Page_32.png","Page_33.png","Page_34.png","Page_35.png","Page_36.png","Page_37.png","Page_38.png","Page_39.png","Page_40.png","Page_41.png","Page_42.png","Page_43.png"]
 };
