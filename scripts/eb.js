886e6b1296702324eebf84a162c893d2a3457539
snrdrmz <durmazsoner@gmail.com>
2014-01-15 14:43:38 +0200

    .

--- scripts/eb.js
+++
@@ -1,5 +0,0 @@
-var slides = {
-	desc: "Elektronik Balastlar",
-	root: "images/Elektronik_Balastlar/",
-	images: ["031.png", "032.png", "033.png"]
-};
--- scripts/fl.js
+++
@@ -1,5 +0,0 @@
-var slides = {
-	desc: "Fluoresan Lambalar",
-	root: "images/Fluoresan_Lambalar/",
-	images: ["024.png", "025.png"]
-};
